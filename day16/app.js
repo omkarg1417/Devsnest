9950f36d58f11223faeecda95a5c9991da400e93
omkarg1417 <67311320+omkarg1417@users.noreply.github.com>
2021-10-12 19:36:27 +0530

    day16

---
+++ day16/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const app = express();
+var path = require('path');
+
+ // To stream video
+app.use('/stream', require("./routes/stream"));
+
+// To stripe payment
+app.use('/stripe', require("./routes/stripe"));
+
+// To search
+app.use('/search', require("./routes/search"));
+
+app.listen(3000, ()=> {
+  console.log("Listening on port 3000");
+});
